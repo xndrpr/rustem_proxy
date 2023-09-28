7aaa421c2e5ecd4241d6482493ec2c6014393b40
uaquax <uaquax@tutanota.com>
2023-09-28 23:27:02 +0800

    More tests

--- tests/test.rs
+++ tests/test.rs
@@ -9,7 +9,21 @@ mod tests {
             host: "127.0.0.1".to_string(),
             port: 61000,
             bypass: "".to_string(),
+            protocol: rustem_proxy::Protocol::SOCKS,
+        });
+        SystemProxy::set(SystemProxy {
+            is_enabled: true,
+            host: "127.0.0.1".to_string(),
+            port: 61001,
+            bypass: "".to_string(),
             protocol: rustem_proxy::Protocol::HTTP,
+        });
+        SystemProxy::set(SystemProxy {
+            is_enabled: true,
+            host: "127.0.0.1".to_string(),
+            port: 61001,
+            bypass: "".to_string(),
+            protocol: rustem_proxy::Protocol::HTTPS,
         });
 
         assert!(true);
